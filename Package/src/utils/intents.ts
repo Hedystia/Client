0d202012c436fc3ece2aa435f4f9dcc3bca809de
Zastinian <zastinian@gmail.com>
2025-04-04 19:37:03 -0600

    feat: Update intents

--- Package/src/utils/intents.ts
+++ Package/src/utils/intents.ts
@@ -1,57 +1,50 @@
-class Intents {
-  static Flags: {
-    Guilds: number;
-    Guild_Members: number;
-    Guild_Moderation: number;
-    Guild_Emojis_And_Stickers: number;
-    Guild_Integrations: number;
-    Guild_Webhooks: number;
-    Guild_Invites: number;
-    Guild_Voice_States: number;
-    Guild_Presences: number;
-    Guild_Messages: number;
-    Guild_Message_Reactions: number;
-    Guild_Message_Typing: number;
-    Direct_Messages: number;
-    Direct_Message_Reactions: number;
-    Direct_Message_Typing: number;
-    Message_Content: number;
-    Guild_Scheduled_Events: number;
-    Auto_Moderation_Configuration: number;
-    Auto_Moderation_Execution: number;
-  };
-  static Default: number;
-  static All: number;
+enum GatewayIntents {
+  guilds = 1 << 0,
+  guildMembers = 1 << 1,
+  guildModeration = 1 << 2,
+  guildExpressions = 1 << 3,
+  guildIntegrations = 1 << 4,
+  guildWebhooks = 1 << 5,
+  guildInvites = 1 << 6,
+  guildVoiceStates = 1 << 7,
+  guildPresences = 1 << 8,
+  guildMessages = 1 << 9,
+  guildMessageReactions = 1 << 10,
+  guildMessageTyping = 1 << 11,
+  directMessages = 1 << 12,
+  directMessageReactions = 1 << 13,
+  directMessageTyping = 1 << 14,
+  messageContent = 1 << 15,
+  guildScheduledEvents = 1 << 16,
+  autoModerationConfiguration = 1 << 20,
+  autoModerationExecution = 1 << 21,
+  guildMessagePolls = 1 << 24,
+  directMessagePolls = 1 << 25,
 
-  convert(flags: number[]): number {
-    return Object.values(flags).reduce((a, b) => a | b, Intents.Default);
-  }
+  allNonPrivileged = GatewayIntents.guilds |
+    GatewayIntents.guildModeration |
+    GatewayIntents.guildExpressions |
+    GatewayIntents.guildIntegrations |
+    GatewayIntents.guildWebhooks |
+    GatewayIntents.guildInvites |
+    GatewayIntents.guildVoiceStates |
+    GatewayIntents.guildMessages |
+    GatewayIntents.guildMessageReactions |
+    GatewayIntents.guildMessageTyping |
+    GatewayIntents.directMessages |
+    GatewayIntents.directMessageReactions |
+    GatewayIntents.directMessageTyping |
+    GatewayIntents.guildScheduledEvents |
+    GatewayIntents.autoModerationConfiguration |
+    GatewayIntents.autoModerationExecution |
+    GatewayIntents.guildMessagePolls |
+    GatewayIntents.directMessagePolls,
+
+  allPrivileged = GatewayIntents.guildMembers |
+    GatewayIntents.guildPresences |
+    GatewayIntents.messageContent,
+
+  all = GatewayIntents.allNonPrivileged | GatewayIntents.allPrivileged,
 }
 
-Intents.Flags = {
-  Guilds: 1 << 0,
-  Guild_Members: 1 << 1,
-  Guild_Moderation: 1 << 2,
-  Guild_Emojis_And_Stickers: 1 << 3,
-  Guild_Integrations: 1 << 4,
-  Guild_Webhooks: 1 << 5,
-  Guild_Invites: 1 << 6,
-  Guild_Voice_States: 1 << 7,
-  Guild_Presences: 1 << 8,
-  Guild_Messages: 1 << 9,
-  Guild_Message_Reactions: 1 << 10,
-  Guild_Message_Typing: 1 << 11,
-  Direct_Messages: 1 << 12,
-  Direct_Message_Reactions: 1 << 13,
-  Direct_Message_Typing: 1 << 14,
-  Message_Content: 1 << 15,
-  Guild_Scheduled_Events: 1 << 16,
-  Auto_Moderation_Configuration: 1 << 20,
-  Auto_Moderation_Execution: 1 << 21,
-};
-
-Intents.Default = 0;
-
-Intents.All = Object.values(Intents.Flags).reduce((a, b) => a | b, Intents.Default);
-
-export default Intents;
+export default GatewayIntents;
