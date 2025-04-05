f24d6cf1a4639e16d5b2ce62a183ced956cdba90
Zastinian <zastinian@gmail.com>
2025-04-04 19:32:30 -0600

    feat: Removed ws

--- Package/src/index.ts
+++ Package/src/index.ts
@@ -2,5 +2,4 @@ export { default as Cache } from "@/utils/cache";
 export { default as Client } from "@/client";
 export { default as Intents } from "@/utils/intents";
 export { default as REST } from "@/rest";
-export { default as WebSocketManager } from "@/ws";
 export * from "@/utils/constants";
