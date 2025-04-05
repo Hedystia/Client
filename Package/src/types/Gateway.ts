a37267cdea99e4b54fc8159cc86b6fbad71b741f
Zastinian <zastinian@gmail.com>
2025-04-04 19:36:37 -0600

    feat: Added gateway type

---
+++ Package/src/types/Gateway.ts
@@ -0,0 +1,28 @@
+import type { ActivityType, PresenceUpdateReceiveStatus } from "discord-api-types/v10";
+import type { default as GatewayIntents } from "@/utils/intents";
+
+interface GatewayActivity {
+  name: string;
+  type: ActivityType;
+  url?: string;
+  state?: string;
+}
+
+export interface Presence {
+  activities?: GatewayActivity[];
+  status?: PresenceUpdateReceiveStatus;
+}
+
+export interface Identify {
+  token: string;
+  properties: {
+    os: string;
+    browser: string;
+    device: string;
+  };
+  compress?: boolean;
+  largeThreshold?: number;
+  shard?: [number, number];
+  presence?: Partial<Pick<Presence, "activities" | "status">>;
+  intents: GatewayIntents;
+}
