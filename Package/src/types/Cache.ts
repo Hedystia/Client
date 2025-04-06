3e2b55ae10e08212e5746f8339aaf166834f26e8
Zastinian <zastinian@gmail.com>
2025-04-06 16:49:54 -0600

    feat: Update cache type

--- Package/src/types/Cache.ts
+++ Package/src/types/Cache.ts
@@ -1,15 +1,14 @@
 import type { APIGuild } from "discord-api-types/v10";
 
-export type CacheAPIGuild = Pick<
-  APIGuild,
-  Exclude<
-    keyof APIGuild,
-    | "stickers"
-    | "region"
+export type CacheAPIGuild = {
+  [K in Exclude<keyof APIGuild, "region">]: K extends
     | "roles"
     | "emojis"
+    | "stickers"
     | "approximate_member_count"
     | "approximate_presence_count"
     | "application_id"
-  >
->;
+    | "region"
+    ? APIGuild[K] | null
+    : APIGuild[K];
+};
