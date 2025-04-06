966403df93565ae11bac1c04e3dbd1caa01e3f99
Zastinian <zastinian@gmail.com>
2025-04-06 13:04:32 -0600

    feat: Added guild structure

---
+++ Package/src/structures/GuildStructure.ts
@@ -0,0 +1,14 @@
+import type { CacheAPIGuild } from "@/types/Cache";
+
+class GuildStructure<T extends CacheAPIGuild = CacheAPIGuild> {
+  constructor(data: T) {
+    Object.assign(this, data);
+  }
+}
+
+export default GuildStructure as new <T extends CacheAPIGuild = CacheAPIGuild>(
+  data: T,
+) => GuildStructure<T> & T;
+
+
+export type GuildStructureInstance = InstanceType<typeof GuildStructure> & CacheAPIGuild;
