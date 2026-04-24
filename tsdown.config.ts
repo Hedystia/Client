527fc80027102c582853ee691ca960a134fc3704
Zastinian <zastinian@gmail.com>
2026-04-24 10:04:23 -0600

    feat: Removed sourcemap

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
   dts: true,
-  sourcemap: true,
+  sourcemap: false,
   treeshake: true,
   clean: true,
   unbundle: true,
