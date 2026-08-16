1a0c60a9f2e7f8dd2c838e52c724492f35657a14
Zastinian <zastinian@gmail.com>
2026-08-15 18:51:15 -0600

    feat: export new managers and option types

--- src/managers/index.ts
+++ src/managers/index.ts
@@ -1,18 +1,37 @@
+export type { ApplicationCommandFetchOptions } from "./ApplicationCommandManager";
+export {
+  ApplicationCommandPermissionManager,
+  default as ApplicationCommandManager,
+} from "./ApplicationCommandManager";
+export { default as ApplicationEmojiManager } from "./ApplicationEmojiManager";
 export { default as AutoModerationRuleManager } from "./AutoModerationRuleManager";
 export { default as ChannelManager } from "./ChannelManager";
 export { default as EmojiManager } from "./EmojiManager";
 export { default as EntitlementManager } from "./EntitlementManager";
+export type { GuildBanCreateOptions, GuildBulkBanOptions } from "./GuildBanManager";
 export { default as GuildBanManager } from "./GuildBanManager";
 export { default as GuildManager } from "./GuildManager";
 export { default as GuildScheduledEventManager } from "./GuildScheduledEventManager";
+export { default as GuildTemplateManager } from "./GuildTemplateManager";
 export { default as IntegrationManager } from "./IntegrationManager";
 export { default as InviteManager } from "./InviteManager";
 export { default as MemberManager } from "./MemberManager";
+export { default as OAuth2Manager } from "./OAuth2Manager";
+export type {
+  PermissionOverwriteOptions,
+  PermissionOverwriteRequestOptions,
+} from "./PermissionOverwriteManager";
+export { default as PermissionOverwriteManager } from "./PermissionOverwriteManager";
+export type { RoleFetchOptions } from "./RoleManager";
 export { default as RoleManager } from "./RoleManager";
+export type { SkuFetchOptions } from "./SkuManager";
+export { default as SkuManager } from "./SkuManager";
 export { default as SoundboardSoundManager } from "./SoundboardSoundManager";
 export { default as StageInstanceManager } from "./StageInstanceManager";
 export { default as StickerManager } from "./StickerManager";
+export { default as StickerPackManager } from "./StickerPackManager";
 export { default as SubscriptionManager } from "./SubscriptionManager";
 export { default as ThreadMemberManager } from "./ThreadMemberManager";
 export { default as UserManager } from "./UserManager";
+export type { WebhookCreateOptions, WebhookEditOptions } from "./WebhookManager";
 export { default as WebhookManager } from "./WebhookManager";
