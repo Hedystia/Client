5163a537010991dab4ebf8ef2e109d7f023b1091
Zastinian <zastinian@gmail.com>
2026-08-15 18:51:24 -0600

    feat: cache threads and thread members on threadListSync

--- src/events/threadListSync.ts
+++ src/events/threadListSync.ts
@@ -1,6 +1,8 @@
 import type { GatewayThreadListSyncDispatchData } from "discord-api-types/v10";
 import type Client from "../client";
+import ChannelStructure from "../structures/ChannelStructure";
 import ThreadListSyncStructure from "../structures/ThreadListSyncStructure";
+import ThreadMemberStructure from "../structures/ThreadMemberStructure";
 
 export default class ThreadListSync {
   client: Client;
@@ -17,6 +19,24 @@ export default class ThreadListSync {
 
   async _patch(data: { d: GatewayThreadListSyncDispatchData }): Promise<void> {
     const packet = data.d;
+
+    for (const thread of packet.threads) {
+      const threadStructure = new ChannelStructure(thread, this.client);
+      this.client.channels._add(threadStructure, { enabled: true, force: false });
+    }
+
+    for (const member of packet.members) {
+      if (!member.id) {
+        continue;
+      }
+      const memberStructure = new ThreadMemberStructure(
+        member,
+        member.id,
+        packet.guild_id,
+        this.client,
+      );
+      this.client.threadMembers._add(memberStructure, { enabled: true, force: false });
+    }
 
     const threadListSyncStructure = new ThreadListSyncStructure(packet, this.client);
     this.client.emit("threadListSync", threadListSyncStructure);
