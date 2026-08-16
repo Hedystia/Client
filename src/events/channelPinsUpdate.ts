b7602dc1ba3caa5abc3f031087bc7793427bd2d7
Zastinian <zastinian@gmail.com>
2026-08-15 18:51:24 -0600

    feat: cache last_pin_timestamp on channel

--- src/events/channelPinsUpdate.ts
+++ src/events/channelPinsUpdate.ts
@@ -17,6 +17,10 @@ export default class ChannelPinsUpdate {
 
   async _patch(data: { d: GatewayChannelPinsUpdateDispatchData }): Promise<void> {
     const packet = data.d;
+    const channel = this.client.channels.get(packet.channel_id);
+    if (channel && packet.last_pin_timestamp !== undefined) {
+      Object.assign(channel, { last_pin_timestamp: packet.last_pin_timestamp });
+    }
 
     const pinsStructure = new ChannelPinsStructure(packet, this.client);
     this.client.emit("channelPinsUpdate", pinsStructure);
