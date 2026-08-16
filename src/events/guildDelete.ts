65305a732542a8b389c7295c01eb0a7834e35a4b
Zastinian <zastinian@gmail.com>
2026-08-15 18:51:23 -0600

    feat: handle unavailable guilds and cleanup channels, roles and messages

--- src/events/guildDelete.ts
+++ src/events/guildDelete.ts
@@ -1,4 +1,8 @@
-import type { APIGuild, GatewayGuildDeleteDispatchData } from "discord-api-types/v10";
+import type {
+  APIGuild,
+  APIUnavailableGuild,
+  GatewayGuildDeleteDispatchData,
+} from "discord-api-types/v10";
 import type Client from "../client";
 import GuildStructure from "../structures/GuildStructure";
 
@@ -17,17 +21,28 @@ export default class GuildDelete {
 
   async _patch(data: { d: GatewayGuildDeleteDispatchData }): Promise<void> {
     const packet = data.d;
+    if (packet.unavailable) {
+      this.client.emit("guildUnavailable", packet as APIUnavailableGuild);
+      return;
+    }
+
     const cachedGuild = this.client.guilds.cache.get(packet.id);
 
     if (cachedGuild) {
       this.client.emit("guildDelete", cachedGuild);
-      this.client.channels._remove(packet.id);
-      this.client.roles._remove(packet.id);
-      this.client.members._remove(packet.id);
-      this.client.guilds._remove(packet.id);
     } else {
       const guildStructure = new GuildStructure(packet as APIGuild, this.client);
       this.client.emit("guildDelete", guildStructure);
     }
+
+    this.client.channels._removeGuild(packet.id);
+    this.client.roles._removeGuild(packet.id);
+    this.client.members._remove(packet.id);
+    for (const [messageId, message] of this.client.messages.entries()) {
+      if (message.guildId === packet.id) {
+        this.client.messages.delete(messageId);
+      }
+    }
+    this.client.guilds._remove(packet.id);
   }
 }
